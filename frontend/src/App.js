816144782b5bbef91e0281ff2b880208fb573593
Cody McKeon <codymckeon@gmail.com>
2023-03-12 13:52:20 -0700

    initalize react app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Alchemist Capitol</a>
+      </header>
+      <main>List properties</main>
+    </div>
+  );
+}
+
+export default App;
