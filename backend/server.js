f6d069adfabe194f5a63326da893027eed5dbf9d
Cody McKeon <codymckeon@gmail.com>
2023-03-12 16:19:20 -0700

    mongodb and render

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,32 @@
 import express from 'express';
 import data from './data.js';
+import path from 'path';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
 
 app.get('/api/properties', (req, res) => {
   res.send(data.properties);
 });
+
+//publish to hosting service
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
