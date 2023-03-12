5757a6b07d258f3571d4534ab88550e760c908da
Cody McKeon <codymckeon@gmail.com>
2023-03-12 14:22:48 -0700

    Add bootstrap and some components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,17 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+
 function App() {
   return (
     <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Nav.Link href="/">
+            <Navbar.Brand>Alchemist Capitol</Navbar.Brand>
+          </Nav.Link>
+        </Container>
+      </Navbar>
       <header className="App-header">
         <a href="/">Alchemist Capitol</a>
       </header>
